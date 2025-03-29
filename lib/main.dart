0b0307afec3fcb74940a4203ba4f930f6385ed98
Abraham Kiplagat <kurerelagat01@.com>
2025-03-29 13:49:46 +0300

    it's testing a button if clicked

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Main(),
-      ),
+      home: ButtonWidget(),
     );
   }
 }
 
-class Main extends StatelessWidget {
+class ButtonWidget extends StatefulWidget {
+  @override
+  _ButtonWidgetState createState() => _ButtonWidgetState();
+}
+
+class _ButtonWidgetState extends State<ButtonWidget> {
+  String buttonText = "Press me";
+
+  void updateText() {
+    setState(() {
+      buttonText = "Button Clicked!";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    String name = "Kiplagat Kurere"; // Fixed null issue
-    return Center(child: Text(name));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(buttonText, key: Key('buttonText')),
+            ElevatedButton(
+              key: Key('clickButton'),
+              onPressed: updateText,
+              child: Text('Click Me'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:testing/main.dart'; // Ensure correct import
+import 'package:testing/main.dart';
 
 void main() {
-  testWidgets('Main widget displays name', (WidgetTester tester) async {
-    // Build MyApp
-    await tester.pumpWidget(MyApp());
+  testWidgets('Button click updates text', (WidgetTester tester) async {
+    // Build the widget
+    await tester.pumpWidget(MaterialApp(home: ButtonWidget()));
 
-    // Verify Main displays the correct text
-    expect(find.text('Kiplagat Kurere'), findsOneWidget);
+    // Verify initial state
+    expect(find.text('Press me'), findsOneWidget);
+    expect(find.text('Button Clicked!'), findsNothing);
+
+    // Tap the button
+    await tester.tap(find.byKey(Key('clickButton')));
+    await tester.pump(); // Rebuild the UI after state change
+
+    // Verify text update
+    expect(find.text('Press me'), findsNothing);
+    expect(find.text('Button Clicked!'), findsOneWidget);
   });
 }
